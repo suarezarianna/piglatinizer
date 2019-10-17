d5eef6a025a9dd8064822cf8712b4bc24321a1b1
suarezarianna <suarezarianna70@gmail.com>
2019-10-17 01:27:36 +0000

    html and javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $( "document" ).ready(function() {
+$("button").click(function(){
+    let text = $("input").val();
+    let words = text.split(" ");
+    let beg1 = words[0].charAt(0);
+    let beg2 = words[1].charAt(0);
 
-
-
+    let end1 = words[0].slice(1, words[0].length);
+    let end2 = words[1].slice(1, words[1].length);
+    $("#result").append(beg2+end1+' '+beg1+end2);
+});
 });
